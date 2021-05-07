743a8700a67009c6db58ed8efb9f69c1cc91aded
shloakr <shloakrathod1@gmail.com>
2021-05-08 01:51:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,4 +37,5 @@ def predict():
     # return render_template('result.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
+    #app.run(debug=True)
